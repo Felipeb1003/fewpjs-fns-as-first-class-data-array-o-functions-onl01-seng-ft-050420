cdd7152104d0fe1eaed370d7ec5c5edc393cdd47
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-08-21 12:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var wakeDog = function(dogName, dogBreed) {
-console.log(`Wake ${dogName} the ${dogBreed}`);
+var wakeDog = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+var leashDog = function(dogName="Byron", dogBreed="poodle") {
+  console.log(``)
 }
 
